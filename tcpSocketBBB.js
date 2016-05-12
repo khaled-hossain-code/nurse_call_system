a6a0b8bd3debb9a42faaba08eeaef64adf877392
khaled <md.khaled.eee@gmail.com>
2016-05-12 12:09:57 +0000

    dynamic client address is generated

--- tcpSocketBBB.js
+++ tcpSocketBBB.js
@@ -1,19 +1,39 @@
 var net = require('net');
-
-var HOST = '192.168.1.6';
-var PORT = 6969;
+var HOST = '192.168.1.6'; //server address to which beaglebone will be connected
+var PORT = 6969; //server port address
 
 var client_socket = net.connect({port:PORT, host:HOST});
+var BBB = client_socket.address();
+/*BBB = {
+    address: '192.168.1.240',
+    family: 'IPv4',
+    port:60285
+}*/
+
+
 
 client_socket.on('connect', function() {
     console.log('connected to server!');
-    console.dir(client_socket.address());
+    console.log("Server => "+HOST+":"+PORT);
+    console.log("Client => "+BBB.address+":"+BBB.port);
 });
 
-process.stdin.pipe(client_socket);
+//process.stdin.pipe(client_socket); //this is for chatting
 
 client_socket.on('data', function(chunk){
     console.log('Server: '+chunk);
+    client_socket.write("I am fine. what about you?");
+});
+
+client_socket.on('end', function(){
+   console.log("disconnected");
+   //TODO have to reconnect
+   
+});
+
+client_socket.on('timeout', function(){
+   console.log("happened timeout due to idle");
+   //TODO have to reconnect
 });
 
 //client_socket.setTimeout(50000, function(){
