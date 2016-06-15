1a145c2045234f95a85d4729f0156a846ee77fdf
khaled <tokyo_emb@yahoo.com>
2016-06-15 04:47:02 +0000

    updated

--- buzzer.js
+++ buzzer.js
@@ -1,7 +1,9 @@
 var b = require('bonescript');
 var buzzerDutyCycle = 0.5;
-var buzzerFreq = 2000;
-var callIndicationSound = "P8_19";
+var buzzerFreq = 1000;
+var callIndicationSound = 'P8_19';
+
+b.pinMode(callIndicationSound,  b.ANALOG_OUTPUT);
 
 soundIndication(1000);
 
@@ -10,9 +12,31 @@ soundIndication(1000);
 //inputs:- delay in milliseconds (the duration of how long the sound will be)
 //outputs:- none
 function soundIndication(milliseconds){ 
-    b.analogWrite(callIndicationSound, buzzerDutyCycle, buzzerFreq);
+    try{
+        console.log("inside try block");
+        //setInterval(function(){console.log("running"), 10000});
+        b.analogWrite(callIndicationSound, buzzerDutyCycle, buzzerFreq, printJSON);
+    }
+    catch(err)
+    {
+        console.log("inside catch err block");
+        //b.analogWrite(callIndicationSound, 0);
+        //b.analogWrite(callIndicationSound, buzzerDutyCycle, buzzerFreq);
+    }
     
     setTimeout(function() {
+        console.log("inside setTimeout block");
         b.analogWrite(callIndicationSound, 0);     // Turn off buzzer after milliseconds time 
     }, milliseconds);
+}
+
+
+function printJSON(x) { 
+    if(x.err)
+    {
+        b.digitalWrite(callIndicationSound, 0);
+        console.log("error happend");
+    }
+    console.log(JSON.stringify(x)); 
+    
 }
