423ce09eb0884767338d91cc2e03be9a9d445885
Md. khaled hossain <md.khaled.eee@gmail.com>
2016-06-16 08:40:25 +0000

    buzzer is ok now

--- buzzer.js
+++ buzzer.js
@@ -1,42 +1,13 @@
 var b = require('bonescript');
 var buzzerDutyCycle = 0.5;
-var buzzerFreq = 1000;
+var buzzerFreq = 2000;
 var callIndicationSound = 'P8_19';
 
 b.pinMode(callIndicationSound,  b.ANALOG_OUTPUT);
 
-soundIndication(1000);
+b.analogWrite(callIndicationSound, buzzerDutyCycle, buzzerFreq);
 
-//this is a function to generate beep
-//Description:- Whenever patient calls nurse its a sound indication to confirm that the call is happend. or any kind of error also generate sound
-//inputs:- delay in milliseconds (the duration of how long the sound will be)
-//outputs:- none
-function soundIndication(milliseconds){ 
-    try{
-        console.log("inside try block");
-        //setInterval(function(){console.log("running"), 10000});
-        b.analogWrite(callIndicationSound, buzzerDutyCycle, buzzerFreq, printJSON);
-    }
-    catch(err)
-    {
-        console.log("inside catch err block");
-        //b.analogWrite(callIndicationSound, 0);
-        //b.analogWrite(callIndicationSound, buzzerDutyCycle, buzzerFreq);
-    }
-    
-    setTimeout(function() {
+setTimeout(function() {
         console.log("inside setTimeout block");
         b.analogWrite(callIndicationSound, 0);     // Turn off buzzer after milliseconds time 
-    }, milliseconds);
-}
-
-
-function printJSON(x) { 
-    if(x.err)
-    {
-        b.digitalWrite(callIndicationSound, 0);
-        console.log("error happend");
-    }
-    console.log(JSON.stringify(x)); 
-    
-}
+    }, 2000);
